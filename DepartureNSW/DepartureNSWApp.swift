29f279ebfe8abc36181e09de8a0f82181e55d112
Terran <terrankroft@gmail.com>
2024-06-08 10:47:42 +1000

    Initial Commit

---
+++ DepartureNSW/DepartureNSWApp.swift
@@ -0,0 +1,17 @@
+//
+//  DepartureNSWApp.swift
+//  DepartureNSW
+//
+//  Created by Terran Kroft on 8/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct DepartureNSWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
